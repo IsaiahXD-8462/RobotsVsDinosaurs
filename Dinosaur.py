d9527a6091645ecce93b6f6d66624839a6af6530
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2022-12-11 17:36:42 -0500

    Added Files

---
+++ Dinosaur.py
@@ -0,0 +1,7 @@
+class Dinosaur
+
+    def __init__(self):
+        self.name = ''
+        self.health_points = []
+        self.defense_power = []
+        self.attack_power = []
---
+++ Robot.py
@@ -0,0 +1,6 @@
+class Robot
+
+        def__init__(self):
+            self.name = ''
+            self.health_points = []
+            self.defense_power = []
