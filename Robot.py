f850ff7deab2754df31da27a8cfb3737a65fb3dd
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-01-09 02:21:44 -0500

    git commit -m "Edit changes"

--- Robot.py
+++ Robot.py
@@ -1,6 +1,8 @@
+from Weapon import Weapon
+
 class Robot
 
-        def__init__(self):
-            self.name = ''
+        def__init__(self, name, health_points, defense_points):
+            self.name = 'name'
             self.health_points = []
             self.defense_power = []
--- Weapon.py
+++ Weapon.py
@@ -2,4 +2,8 @@ class Weapon
 
     def __init__(self, name, attack_power):
         self.name = ''
-        self.attack_power
+        self.attack_power = []
+
+    def name(self):
+        self.name = input('Weapon name:')
+        print(self.name)
